3991bca6b50ab98769f10f409efed7b7041bcf5c
TayyabArifeen <tayyab.arifeen@tu-ilmenau.de>
2025-04-30 15:52:00 +0200

    Add project files.

---
+++ Code Part/Exercise1.cs
@@ -0,0 +1,34 @@
+﻿using assignmentQuestions.Code_Part;
+using System;
+
+namespace assignmentQuestions
+{
+    class Exercise1
+    {
+        static void Main(string[] args)
+        {
+            var randNum = new Random();
+            for (int i = 1; i <= 100; i++)
+            {
+                int number = randNum.Next(1, 101);
+                Console.Write($"{i}: {number}");
+                if (number % 7 == 0) Console.Write(" Lucky Number!");
+                Console.WriteLine();
+                if (i % 5 == 0) Console.WriteLine("******");
+            }
+
+            // Exercise3 part 1
+
+            A a1 = new D("D-Class");
+            a1.PrintName();
+
+            A a2 = new B();
+            a2.PrintName();
+
+            C c = new C();
+            c.PrintName("Hello from C");
+
+            // A obj = new A(); Not allowed!  Reference to question1 of exercise 4.
+        }
+    }
+}
---
+++ Code Part/Exercise3.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignmentQuestions.Code_Part
+{
+    public class Exercise3
+    {
+    }
+    abstract class A
+    {
+        protected string Name;
+
+        public abstract void PrintName();
+    }
+
+    class B : A
+    {
+        private void PrintName(string message)
+        {
+            Console.WriteLine("B: " + message);
+        }
+
+        public override void PrintName()
+        {
+            Console.WriteLine("B: " + Name);
+        }
+    }
+
+    class C : B
+    {
+        public void PrintName(string message)
+        {
+            Console.WriteLine("C: " + message);
+        }
+
+    }
+
+    class D : A
+    {
+        public D(string name)
+        {
+            Name = name;
+        }
+
+        public override void PrintName()
+        {
+            Console.WriteLine("D: " + Name);
+        }
+    }
+}
